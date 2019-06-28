41c7c838e2e098552c64cc0d68136aaa0bdb99c9
James <hayward.jp@gmail.com>
2019-06-28 10:28:02 +0100

    Done.

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,15 @@
 class Bird < ApplicationRecord
+   has_many :sightings
+   has_many :locations, through: :sightings
+
+   def index
+      birds = Bird.all
+      render json: birds
+   end
+
+   def show
+      bird = Bird.find_by(id: params[:id])
+      render json: bird
+   end
+
 end
